ab2c1212a11e4248149ecc8befd755de7a0f28f0
Leonardo <leonardo.dalmeida@br.ey.com>
2024-03-14 11:07:04 -0300

    correcao

--- pasta/codigo_simples_bug.py
+++ pasta/codigo_simples_bug.py
@@ -1,6 +1,7 @@
 def somar_numeros(a, b):
     # Esta função deve retornar a soma de a e b
     resultado = a + b  # Erro: 'c' não está definido
+    
     return resultado
 
 print(somar_numeros(5, 3))
